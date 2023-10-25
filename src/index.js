2686318dff99732766ee44ba03a6d73d6b06aa13
carlDguez19 <carldominguez19@gmail.com>
2023-10-24 20:24:00 -0600

    found a way clear page and add new content within one evenListener

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { liContact } from "./restBanner";
 import { bannerMain } from "./restBanner";
 import { restLandingPage } from "./restHome";
 
-// const content = document.querySelector('#content');
-// console.log("this is working lollipop");
-// const testDiv = document.createElement('div');
-// testDiv.classList.add('testDivin');
-// content.appendChild(testDiv);
-bannerMain();
+liHome.addEventListener('click', function(){
+    clearContent();
+    restLandingPage();
+});
 
+bannerMain();
 restLandingPage();
+
+function clearContent(){
+    document.querySelector('#content').innerHTML = "";
+}
+
 console.log("THis is jsut a test blaaa popcorn lollipop pan");
